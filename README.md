2eb787c44b8fb695669cf32f9dbc071c0068b3c6
Ryan Briones <ryan.briones@brionesandco.com>
2013-11-21 08:40:31 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Travis' List
+
+A craigslist clone to demonstrate working with Travis CI and a Behavior-driven Development style of testing.
+
+## Contact
+
+Ryan Briones <ryan@devbootcamp.com>
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravisList::Application.config.secret_token = '8cb2295ed4237810862e68454da08d5c20c39bbaec383a29d32963a050307aeab31fbfc78f87688c04d4ea2c3f840b959f8055c096efa3a9b4bd280f810910c8'
